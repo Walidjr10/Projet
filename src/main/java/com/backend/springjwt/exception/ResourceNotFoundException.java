66f28e896e9c6484b8a3c0200296bb75cbe5c010
oualid <walidsaitama@gmail.com>
2024-01-06 19:55:41 +0100

    Ajout de gestion d'exception

--- src/main/java/com/backend/springjwt/exception/ResourceNotFoundException.java
+++ src/main/java/com/backend/springjwt/exception/ResourceNotFoundException.java
@@ -6,14 +6,17 @@ public class ResourceNotFoundException extends RuntimeException{
 
     public ResourceNotFoundException() {
         super();
+
     }
 
     public ResourceNotFoundException(String message) {
         super(message);
+
     }
 
     public ResourceNotFoundException(String message, Throwable cause) {
         super(message, cause);
+        
     }
 
 }
